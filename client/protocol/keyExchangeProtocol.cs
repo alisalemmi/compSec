67e28cddda9543054dc47f064e29e921bf78b32c
علی سالمی <alisalemmi@outlook.com>
2022-01-31 13:21:56 +0330

    مرحله دوم تبادل کلید

--- client/protocol/keyExchangeProtocol.cs
+++ client/protocol/keyExchangeProtocol.cs
@@ -3,6 +3,7 @@ class KeyExchangeProtocol
   public RSA clientRsa = new RSA();
   private RSA serverRsa = new RSA("../../server/key/public.pem");
   private uint nonceA;
+  private uint nonceB;
 
   public string startHandshake()
   {
@@ -10,6 +11,22 @@ class KeyExchangeProtocol
     string encriptedNonce = this.serverRsa.encrypt(this.nonceA.ToString());
 
     return $"{encriptedNonce}.{this.clientRsa.publicKey}";
+  }
+
+  public void verifyServer(string data)
+  {
+    string[] args = data.Trim().Split(".");
+
+    var na = Convert.ToUInt32(this.clientRsa.decrypt(args[0]));
+    var nb = Convert.ToUInt32(this.clientRsa.decrypt(args[1]));
+
+    if (na != this.nonceA + 1)
+      throw new Exception("invalid nonce A");
+
+    this.nonceB = nb;
+
+    if (!this.serverRsa.verify(args[2], $"{nonceB}.{this.clientRsa.publicKey}"))
+      throw new Exception("invalid signature");
   }
 
   private uint generateNonce()
